58248fc03178c453d0a7141b532227d5d556da08
israel2014 <israel_frp@hotmail.com>
2014-04-07 20:19:57 -0300

    Criando Pessoas

--- pessoas/views.py
+++ pessoas/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from pessoas.models import Pessoa
 
-# Create your views here.
+
 def index(request):
     return render(request, 'index.html')
+
+def pessoaListar(request):
+    pessoas = Pessoa.objects.all()[0:10]
+    return render(request, 'pessoas/listaPessoas.html', {'pessoas': pessoas})
